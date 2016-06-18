4a83ceb86512a60868ba9345d25a08d23266f216
Igor Rendulic <igor.amplio@gmail.com>
2016-06-17 19:14:03 -0700

    Initial Commit

---
+++ smokeless/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  smokeless
+//
+//  Created by Igor Test on 17/06/16.
+//  Copyright © 2016 Igor Rendulic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ smokeless/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  smokeless
+//
+//  Created by Igor Test on 17/06/16.
+//  Copyright © 2016 Igor Rendulic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
